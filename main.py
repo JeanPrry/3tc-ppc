65800ab2b33a84607bf51b96a3d311ce8c1e6cb9
JeanPrry <31445149+JeanPrry@users.noreply.github.com>
2023-01-18 16:14:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import threading
+from multiprocessing import Value, Array, Process
+import random
+import signal
+import os
+
+
+EVENT1 = 0.025
+EVENT2 = 0.7956
+NOEVENT = 0
+
+
+def weather_Process(weather):
+    while True:
+        for i in range(len(weather)):
+            weather[i] = random.random()
+
+
+def market_Process(weather):
+    energy_price = Value("d",0.0)
+    while True:
+        weather_conditions = weather
+        
+        signal.signal(signal.SIGUSR1, handler)
+
+        pexternal = Process(target=external_Process, args=())
+        pexternal.start()
+    
+        pexternal.join()
+
+
+def handler(sig,frame):
+    if sig == signal.SIGUSR1:
+        print("1")
+    elif sig == signal.SIGUSR2:
+        print("2")
+
+
+def external_Process():
+    events = [NOEVENT, EVENT1, EVENT2]
+    events_prob = [9, 2, 2]
+    while True:
+        event = random.choices(events, weights=events_prob, k=1) #Choose one event from the list of events
+        
+        if event[0] != 0:
+            if event[0] == EVENT1:
+                os.kill(os.getppid(), signal.SIGUSR1)
+            elif event[0] == EVENT2:
+                os.kill(os.getppid(), signal.SIGUSR2)
+
+
+if __name__ == "__main__" :
+
+    weather_conditions = Array("d", range(2))
+
+    pweather = Process(target=weather_Process, args=(weather_conditions,))
+    pmarket = Process(target=market_Process, args=(weather_conditions,))
+    
+
+    pweather.start()
+    pmarket.start()
+
+    pweather.join()
+    pmarket.join()
+    
