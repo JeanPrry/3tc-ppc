5f4d5ed6ab491f1b3f776324f42cb950b22a5542
JeanPrry <31445149+JeanPrry@users.noreply.github.com>
2023-01-26 09:36:39 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,154 +1,52 @@
-module Test exposing (..)
+import socket
+import select
+import threading
 
-import Browser
-import Html exposing (..)
-import Html.Attributes exposing (style, value, type_)
-import Html.Events exposing (..)
-import Http
-import Json.Decode exposing (Decoder, map2, list, field, string)
+HOST = "localhost"
+PORT = 8889
 
--- MAIN
-
-main =
-  Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
-
--- MODEL
-
-type Model
-  = Failure
-  | Loading
-  | Success (List Word)
+global serve
+serve = True
 
 
-type alias Word =
-    { word : String
-    , meanings : List Meaning
-    }
+def handler_client(list_clients):
+    data = ""
+    while data != "end":
+        if not len(data):
+            for client in list_clients:
+                data = "Enter"
+                client.sendall(data.encode())
+        if data == "external event":
+            for client in list_clients:
+                client.sendall(data.encode())
 
-type alias Meaning =
-    { partOfSpeech : String
-    , definitions : List Definition
-    }
+        data = input()
 
-type alias Definition =
-    { definition : String
-    }
+    for client in list_clients: #Terminate connection
+        client.sendall(data.encode())
 
-init : () -> (Model, Cmd Msg)
-init _ =
-  (Loading, getWord)
-
--- UPDATE
-
-type Msg
-  = GotWord (Result Http.Error (List Word))
-  | CheckWord String
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-    case msg of
-        GotWord (Ok words) ->
-            (Success words, Cmd.none)
-        GotWord (Err error) ->
-            (Failure, Cmd.none)
-        CheckWord entered ->
-            (model, Cmd.none)
-
--- SUBSCRIPTIONS
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
-
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ h2 [] [ text "Word Definitions" ]
-    , viewWord model
-    , input [ type_ "text", onInput CheckWord] []
-    , viewWordMatch model
-    ]
+    global serve
+    serve = False
 
 
+if __name__ == "__main__" :
 
-viewWord : Model -> Html Msg
-viewWord model =
-  case model of
-    Failure ->
-      div []
-        [ text "I could not load the word for some reason. "        ]
+    serve = True
+    list_clients = []
 
-    Loading ->
-      text "Loading..."
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
-    Success words ->
-        div [] (List.map viewWordMeaning words)
+        server_socket.setblocking(False)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(2)
 
-viewWordMeaning : Word -> Html Msg
-viewWordMeaning word =
-    div []
-        [ 
-           ul [] (List.map viewMeaning word.meanings)
-        ]
+        thread = threading.Thread(target=handler_client, args=(list_clients,))
+        thread.start()
 
-viewMeaning : Meaning -> Html Msg
-viewMeaning meaning =
-    li []
-        [ text meaning.partOfSpeech        , ul [] (List.map viewDefinition meaning.definitions)
-        ]
+        while serve:
+            readable, writable, error = select.select([server_socket], [], [], 1)
+            if server_socket in readable:
+                client_socket, address = server_socket.accept()
+                list_clients.append(client_socket)
 
-viewDefinition : Definition -> Html Msg
-viewDefinition def =
-    li [] [ text def.definition ]
-
-viewWordMatch : Model -> Html Msg
-viewWordMatch model =
-  case model of
-    Success words ->
-        case words of
-            word::_ ->
-                case (word.word == enteredWord) of
-                    True -> text "You found the word!"
-                    False -> text "Pute"
-            _ -> text ""
-    _ -> text ""
-
-enteredWord : String
-enteredWord = ""
-
--- HTTP
-
-getWord : Cmd Msg
-getWord =
-    Http.get
-    { url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
-    , expect = Http.expectJson GotWord descriptionDecoder
-    }
-
---Decoder
-descriptionDecoder : Decoder (List Word)
-descriptionDecoder = Json.Decode.list wordDecoder
-
-wordDecoder : Decoder Word
-wordDecoder =
-    map2 Word
-        (field "word" string)
-        (field "meanings" (Json.Decode.list meaningDecoder))
-
-meaningDecoder : Decoder Meaning
-meaningDecoder =
-    map2 Meaning
-        (field "partOfSpeech" string)
-        (field "definitions" (Json.Decode.list definitionDecoder))
-
-definitionDecoder : Decoder Definition
-definitionDecoder =
-    Json.Decode.map Definition
-        (field "definition" string)
+        thread.join()
