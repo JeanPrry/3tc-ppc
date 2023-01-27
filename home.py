272fc0283f2403df03e34ef011534570dd12c16e
JeanPrry <jeanpeaurroy@gmail.com>
2023-01-27 18:48:04 +0100

    implémentatiion de la queue

--- home.py
+++ home.py
@@ -19,12 +19,12 @@ def q_server(homes, run): # Gestion de la queue
 
         for home in homes:
 
-            energy_left = home.production_rate - home.consumption_rate
+            energy_left = home.production.value - home.consumption.value
 
             if energy_left > 0: # Si on a de l'énergie en trop
                 if home.policy == 1:    # On donne tout le temps
                     mq_offre.send(str(energy_left).encode())
-                    home.production_rate -= energy_left
+                    home.production.value -= energy_left
 
                 elif home.policy == 2:  # On vend direct au market
                     pass
@@ -36,11 +36,11 @@ def q_server(homes, run): # Gestion de la queue
                         energy_to_send = int(message)
                         if energy_to_send > energy_left:    # Si on a moins d'énergie que demandé
                             mq_offre.send(str(energy_left).encode())
-                            home.production_rate -= energy_left
+                            home.production.value -= energy_left
 
                         else:   # Si on a plus d'énergie que demandé
                             mq_offre.send(str(energy_to_send).encode())
-                            home.production_rate -= energy_to_send
+                            home.production.value -= energy_to_send
 
                     else:
                         # on envoie au market
@@ -54,9 +54,9 @@ def q_server(homes, run): # Gestion de la queue
                 energy_received = int(energy_received.decode())
 
                 if energy_received < deficit:  # Si on a recu moins que nécessaire
-                    home.production_rate += energy_received
+                    home.production.value += energy_received
                 else:   # Si on a recu plus que nécessaire
-                    home.production_rate += deficit
+                    home.production.value += deficit
                     mq_offre.send(str(energy_received - deficit).encode())  # On renvoie l'énergie en trop
 
     mq_demande.remove()
@@ -95,8 +95,8 @@ class Home:
 
     def __init__(self, barrier):
 
-        self.consumption_rate = random.randint(1, 100)
-        self.production_rate = random.randint(1, 100)
+        self.consumption = Value("i", random.randint(1, 100))
+        self.production = Value("i", random.randint(1, 100))
         self.policy = 1
 
         tcp_main = threading.Thread(target=han_tcp_main, args=(HOST, PORT_MAIN, run, barrier))
@@ -129,8 +129,8 @@ x1 = Array("d", range(len(home_list)))
 x2 = Array("d", [i + largeur for i in x1])
 
 for i in range(len(home_list)):
-    y1[i] = home_list[i].consumption_rate
-    y2[i] = home_list[i].production_rate
+    y1[i] = home_list[i].consumption.value
+    y2[i] = home_list[i].production.value
 
 
 def animate():
@@ -151,33 +151,30 @@ def update(num, ax):
 
 
 p = Process(target=animate, args=())
+p_q = Process(target=q_server, args=(home_list, run))
 
 p.start()
-surplus = Queue()
-carence = Queue()
+p_q.start()
+
 
 while run.value:
+
     while next.value and run.value:
         pass
-    if run.value: #dans le cas où run passe à false pendant la boucle du dessus
+    if run.value: # dans le cas où run passe à false pendant la boucle du dessus
         barrier.wait()
         next.value = 1
-        for home in home_list:
-            if home.consumption_rate < home.production_rate:
-                surplus.put(home)
-            else:
-                carence.put(home)
-        print(next.value)
-        #print(surplus.qsize())
-        #print(carence.qsize())
-        surplus = Queue()
-        carence = Queue()
+
+        print(home_list[0].production, " , ", home_list[0].consumption.value)
+        print()
+
         i = 0
         for h in home_list:
-            h.consumption_rate = random.randint(1, 100)
-            h.production_rate = random.randint(1, 100)
-            y1[i] = home_list[i].consumption_rate
-            y2[i] = home_list[i].production_rate
+            h.consumption.value = random.randint(1, 100)
+            h.production.value = random.randint(1, 100)
+            y1[i] = home_list[i].consumption.value
+            y2[i] = home_list[i].production.value
             i += 1
 
 p.join()
+p_q.join()
