15f5b54a3b604c667c2fe1f02aaf1b8ebfffe1ba
Nitsugu81 <122277644+Nitsugu81@users.noreply.github.com>
2023-01-31 15:26:35 +0100

    Update market.py

--- market.py
+++ market.py
@@ -4,7 +4,7 @@ import threading
 import os
 import signal
 import random
-from multiprocessing import Process, Array, Value
+from multiprocessing import Process, Array, Value, Barrier
 import select
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -95,7 +95,7 @@ def handler_signals(sig, frame):
         external_impact = 5
 
 
-def han_market(run):
+def han_market(run,lock1, lock2, quantite_achete, quantite_vendue):
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
@@ -109,7 +109,7 @@ def han_market(run):
             readable, writable, error = select.select([server_socket], [], [], 1)
             if server_socket in readable and len(threads) <= 10:
                 client_socket, address = server_socket.accept()
-                thread = threading.Thread(target=han_client_market, args=(client_socket, run))
+                thread = threading.Thread(target=han_client_market, args=(client_socket, run, lock1, lock2, quantite_achete, quantite_vendue))
                 threads.append(thread)
                 thread.start()
 
@@ -117,15 +117,19 @@ def han_market(run):
             t.join()
 
 
-def han_client_market(client, run):
-
+def han_client_market(client, run, lock1,lock2, quantite_achete, quantite_vendue):
+    
     while run.value:
         data = client.recv(1024).decode()
         data = float(data)
         if data > 0:
             print("vend ", data)
+            with lock1 :
+                quantite_vendue.value+=float(data)
         elif data < 0:
             print("achete ", data)
+            with lock2 :
+                quantite_achete.value+=float(data)
 
 
 EVENT1 = 0.025
@@ -141,6 +145,11 @@ day = Value("i", 0)
 energy_price = Value("d", 50)
 next = Value("i", 0)
 
+quantite_achete =Value("d", 0)
+quantite_vendue =Value("d", 0)
+lock1 = threading.Lock()
+lock2 = threading.Lock()
+
 list_price = Array("d", [0] * 365)
 list_price[0] = energy_price.value
 
@@ -153,7 +162,7 @@ def animate():
     fig, axs = plt.subplots(2, 1)
     line1, = axs[0].plot(range(365), list_price)
     axs[0].set_ylabel("Price")
-    axs[0].set_ylim(0, 300)
+    axs[0].set_ylim(-100, 100)
     line2, = axs[1].plot(range(365), list_temp)
     axs[1].set_ylabel("Temperature")
     axs[1].set_ylim(-5, 35)
@@ -172,7 +181,7 @@ def update(num, line1, line2):
 
 pweather = Process(target=weather_process, args=(weather_conditions, weather_change, run))
 thread_main = threading.Thread(target=han_main, args=(weather_change, run, new_external))
-thread_market = threading.Thread(target=han_market, args=(run, ))
+thread_market = threading.Thread(target=han_market, args=(run,lock1, lock2, quantite_achete, quantite_vendue))
 process_external = Process(target=external_process, args=(new_external, run))
 process_plot = Process(target=animate, args=())
 
@@ -181,7 +190,7 @@ thread_market.start()
 pweather.start()
 process_external.start()
 process_plot.start()
-
+b = Barrier(2)
 signal.signal(signal.SIGUSR1, handler_signals)
 signal.signal(signal.SIGUSR2, handler_signals)
 signal.signal(signal.SIGINT, handler_signals)
@@ -193,8 +202,11 @@ while run.value:
     if run.value: #dans le cas où run passe à false pendant la boucle du dessus
         day.value += 1
         next.value = 1
-        energy_price.value = 0.998 * energy_price.value + 2 * weather_conditions[0] - 0.02 * weather_conditions[1] + 0.1 * external_impact
-
+        print("somme achat", quantite_achete.value)
+        print("somme vente", quantite_vendue.value)
+        energy_price.value = 1.02 * energy_price.value + 0.002 * weather_conditions[0] - 0.002 * weather_conditions[1] + 0.1 * external_impact
+        quantite_vendue.value = 0
+        quantite_achete.value = 0
         list_price[day.value % 365] = energy_price.value
 
         #print("At day ", day.value, ", energy price : ", energy_price.value)
